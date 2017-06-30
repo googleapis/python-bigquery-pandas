30c424865238f88ebeb8e10b2985c1b6e4b26871
Javier Domingo Cansino <javierdo1@gmail.com>
2017-06-30 09:02:18 +0100

    Fixing linting

--- pandas_gbq/tests/test_gbq.py
+++ pandas_gbq/tests/test_gbq.py
@@ -1092,11 +1092,14 @@ class TestToGBQIntegrationWithServiceAccountKeyPath(object):
         assert schema == test_schema
 
     def test_generate_repeated_schema(self):
-        df = DataFrame([{'A': [1, 2, 3], 'B': ['1', '2']}, {'A': [3, 5], 'B': ['8', '3']}])
+        df = DataFrame([{'A': [1, 2, 3], 'B': ['1', '2']},
+                        {'A': [3, 5], 'B': ['8', '3']}])
         schema = gbq._generate_bq_schema(df)
 
-        test_schema = {'fields': [{'name': 'A', 'type': 'INTEGER', 'mode': 'REPEATED'},
-                                  {'name': 'B', 'type': 'STRING', 'mode': 'REPEATED'}]}
+        test_schema = {'fields': [
+            {'name': 'A', 'type': 'INTEGER', 'mode': 'REPEATED'},
+            {'name': 'B', 'type': 'STRING', 'mode': 'REPEATED'}
+        ]}
 
         assert schema == test_schema
 
