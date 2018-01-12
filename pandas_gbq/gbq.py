0434e34d57d61296c9df09139f32220efbbdc76f
Maximilian Roos <m@maxroos.com>
2018-01-12 10:03:40 -0500

    try forcing utf-8 encoding

--- pandas_gbq/gbq.py
+++ pandas_gbq/gbq.py
@@ -581,7 +581,7 @@ class GbqConnector(object):
                 self._print("\rLoad is {0}% Complete".format(
                     ((total_rows - remaining_rows) * 100) / total_rows))
 
-                body = BytesIO('{}\n'.format('\n'.join(rows)).encode())
+                body = BytesIO('{}\n'.format('\n'.join(rows)).encode('utf-8'))
 
                 try:
                     self.client.load_table_from_file(
