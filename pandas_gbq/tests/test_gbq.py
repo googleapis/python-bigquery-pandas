69147dd6fb45d7699e135195f0abea621dbedf58
rhoboro <rhoboro@gmail.com>
2018-03-13 15:09:11 +0900

    add a test for extact timestamp

--- pandas_gbq/tests/test_gbq.py
+++ pandas_gbq/tests/test_gbq.py
@@ -1389,6 +1389,30 @@ class TestToGBQIntegration(object):
                        private_key=self.credentials,
                        table_schema=test_schema)
 
+    def test_upload_data_with_timestamp(self):
+        test_id = "21"
+        test_size = 6
+        df = DataFrame(np.random.randn(test_size, 4), index=range(test_size),
+                       columns=list('ABCD'))
+        test_timestamp = datetime.now(pytz.timezone('UTC'))
+        df['times'] = test_timestamp
+
+        gbq.to_gbq(
+            df, self.destination_table + test_id,
+            _get_project_id(),
+            private_key=self.credentials)
+
+        result_df = gbq.read_gbq("SELECT * FROM {0}".format(
+            self.destination_table + test_id),
+            project_id=_get_project_id(),
+            private_key=self.credentials)
+
+        assert len(result_df) == test_size
+
+        expected = df['times'].astype('M8[ns]').sort_values()
+        result = result_df['times'].astype('M8[ns]').sort_values()
+        tm.assert_numpy_array_equal(expected.values, result.values)
+
     def test_list_dataset(self):
         dataset_id = self.dataset_prefix + "1"
         assert dataset_id in self.dataset.datasets()
