9edd6099bcaeabd5c9b3cc64a992905112620ab6
Maximilian Roos <m@maxroos.com>
2018-03-31 15:30:17 -0400

    remove pins

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ def readme():
 
 INSTALL_REQUIRES = [
     'pandas',
-    'google-auth>=1.4.1',
-    'google-auth-oauthlib>=0.0.1',
+    'google-auth',
+    'google-auth-oauthlib',
     'google-cloud-bigquery>=0.29.0',
 ]
 
