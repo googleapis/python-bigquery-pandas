041ce177f0eef200939d2ea2ca1248a46ba87b5c
Tim Swast <swast@google.com>
2018-04-10 15:38:23 -0700

    CLN: move _version.py back for versioneer

--- pandas_gbq/__init__.py
+++ pandas_gbq/__init__.py
@@ -1,6 +1,6 @@
 from .gbq import to_gbq, read_gbq  # noqa
 
-from .version import get_versions
+from ._version import get_versions
 versions = get_versions()
 __version__ = versions.get('closest-tag', versions['version'])
 __git_revision__ = versions['full-revisionid']
