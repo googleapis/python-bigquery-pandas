b6295ada491ec2862f63a7493b8489e511a70636
Tim Swast <swast@google.com>
2021-08-18 16:03:43 -0500

    remove one-time script

--- convert_changelog.py
+++
@@ -1,9 +0,0 @@
-import re
-
-
-with open("docs/source/changelog.rst") as f:
-    text = f.read()
-
-# :issue:`312` -> https://github.com/googleapis/python-bigquery-pandas/issues/312
-c = re.compile(r":issue:`([0-9]+)`", flags=re.MULTILINE)
-print(re.sub(c, r"[#\1](https://github.com/googleapis/python-bigquery-pandas/issues/\1)", text))
