4aece99f7d33592889743ab9e1b7ed52fb6835d4
Tim Swast <swast@google.com>
2021-10-28 09:28:38 -0500

    update minimum numpy

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ description = "Google BigQuery connector for pandas"
 release_status = "Development Status :: 4 - Beta"
 dependencies = [
     "setuptools",
+    "numpy>=1.16.6",
     "pandas>=0.23.2",
     "pyarrow >=3.0.0, <7.0dev",
     "pydata-google-auth",
