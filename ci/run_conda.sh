8dd8279044500c2a4b04288073c81ab78a605676
Tim Swast <swast@google.com>
2021-12-08 09:50:51 -0600

    test: remove redundant NIGHTLY run from CircleCI

--- ci/run_conda.sh
+++ ci/run_conda.sh
@@ -9,7 +9,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 # Install dependencies using Conda
 
 conda config --set always_yes yes --set changeps1 no
-conda config --add channels pandas
 conda config --add channels conda-forge
 conda update -q conda
 conda info -a
@@ -17,14 +16,7 @@ conda create -q -n test-environment python=$PYTHON
 source activate test-environment
 REQ="ci/requirements-${PYTHON}-${PANDAS}"
 conda install -q --file "$REQ.conda";
-
-if [[ "$PANDAS" == "NIGHTLY" ]]; then
-  conda install -q numpy pytz python-dateutil;
-  PRE_WHEELS="https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com";
-  pip install --pre --upgrade --timeout=60 -f $PRE_WHEELS pandas;
-else
-  conda install -q pandas=$PANDAS;
-fi
+conda install -q pandas=$PANDAS;
 
 python setup.py develop --no-deps
 
