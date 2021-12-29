11126a6e342422f530c57d4f59738231e4c5ad83
Tim Swast <swast@google.com>
2021-12-29 15:21:57 -0600

    add failing test for desired fix

--- tests/system/test_read_gbq.py
+++ tests/system/test_read_gbq.py
@@ -26,6 +26,7 @@ SELECT
   date_col,
   datetime_col,
   float_col,
+  int64_col,
   numeric_col,
   string_col,
   time_col,
@@ -57,6 +58,13 @@ INNER JOIN
       STRUCT(3 AS row_num, 0.0 AS float_col) ]) AS `floats`
 INNER JOIN
   UNNEST([
+      -- 2 ^ 63 - 1, but in hex to avoid intermediate overlfow.
+      STRUCT(1 AS row_num, 0x7fffffffffffffff AS int64_col),
+      STRUCT(2 AS row_num, -1 AS in64_col),
+      -- -2 ^ 63, but in hex to avoid intermediate overlfow.
+      STRUCT(3 AS row_num, -0x8000000000000000 AS int64_col) ]) AS `ints`
+INNER JOIN
+  UNNEST([
       STRUCT(1 AS row_num, CAST('123.456789' AS NUMERIC) AS numeric_col),
       STRUCT(2 AS row_num, CAST('-123.456789' AS NUMERIC) AS numeric_col),
       STRUCT(3 AS row_num, CAST('999.999999' AS NUMERIC) AS numeric_col) ]) AS `numerics`
@@ -80,6 +88,7 @@ WHERE
   AND `bools`.row_num = `bytes`.row_num
   AND `bools`.row_num = `datetimes`.row_num
   AND `bools`.row_num = `floats`.row_num
+  AND `bools`.row_num = `ints`.row_num
   AND `bools`.row_num = `numerics`.row_num
   AND `bools`.row_num = `strings`.row_num
   AND `bools`.row_num = `times`.row_num
@@ -117,6 +126,9 @@ ORDER BY row_num ASC
                         dtype="datetime64[ns]",
                     ),
                     "float_col": [1.125, -2.375, 0.0],
+                    "int64_col": pandas.Series(
+                        [(2 ** 63) - 1, -1, -(2 ** 63)], dtype="Int64"
+                    ),
                     "numeric_col": [
                         decimal.Decimal("123.456789"),
                         decimal.Decimal("-123.456789"),
@@ -396,287 +408,88 @@ ORDER BY row_num ASC
                 reason="BIGNUMERIC not supported in this version of google-cloud-bigquery",
             ),
         ),
-        # TODO: test with extreme DATE/DATETIME/TIMESTAMPS
+        pytest.param(
+            """
+SELECT
+  dates.row_num AS row_num,
+  date_col,
+  datetime_col,
+  timestamp_col
+FROM
+  UNNEST([
+      STRUCT(1 AS row_num, DATE(1, 1, 1) AS date_col),
+      STRUCT(2 AS row_num, DATE(9999, 12, 31) AS date_col),
+      STRUCT(3 AS row_num, DATE(2262, 4, 12) AS date_col) ]) AS `dates`
+INNER JOIN
+  UNNEST([
+      STRUCT(1 AS row_num, DATETIME('0001-01-01 00:00:00.000000') AS datetime_col),
+      STRUCT(2 AS row_num, DATETIME('9999-12-31 23:59:59.999999') AS datetime_col),
+      STRUCT(3 AS row_num, DATETIME('2262-04-11 23:47:16.854776') AS datetime_col) ]) AS `datetimes`
+INNER JOIN
+  UNNEST([
+      STRUCT(1 AS row_num, TIMESTAMP('0001-01-01 00:00:00.000000') AS timestamp_col),
+      STRUCT(2 AS row_num, TIMESTAMP('9999-12-31 23:59:59.999999') AS timestamp_col),
+      STRUCT(3 AS row_num, TIMESTAMP('2262-04-11 23:47:16.854776') AS timestamp_col) ]) AS `timestamps`
+WHERE
+  `dates`.row_num = `datetimes`.row_num
+  AND `dates`.row_num = `timestamps`.row_num
+ORDER BY row_num ASC
+            """,
+            pandas.DataFrame(
+                {
+                    "row_num": pandas.Series([1, 2, 3], dtype="Int64"),
+                    "date_col": pandas.Series(
+                        [
+                            datetime.date(1, 1, 1),
+                            datetime.date(9999, 12, 31),
+                            datetime.date(2262, 4, 12),
+                        ],
+                        dtype="object",
+                    ),
+                    "datetime_col": pandas.Series(
+                        [
+                            datetime.datetime(1, 1, 1, 0, 0, 0, 0),
+                            datetime.datetime(9999, 12, 31, 23, 59, 59, 999999),
+                            # One microsecond more than pandas.Timestamp.max.
+                            datetime.datetime(2262, 4, 11, 23, 47, 16, 854776),
+                        ],
+                        dtype="object",
+                    ),
+                    "timestamp_col": pandas.Series(
+                        [
+                            datetime.datetime(
+                                1, 1, 1, 0, 0, 0, 0, tzinfo=datetime.timezone.utc
+                            ),
+                            datetime.datetime(
+                                9999,
+                                12,
+                                31,
+                                23,
+                                59,
+                                59,
+                                999999,
+                                tzinfo=datetime.timezone.utc,
+                            ),
+                            # One microsecond more than pandas.Timestamp.max.
+                            datetime.datetime(
+                                2262,
+                                4,
+                                11,
+                                23,
+                                47,
+                                16,
+                                854776,
+                                tzinfo=datetime.timezone.utc,
+                            ),
+                        ],
+                        dtype="object",
+                    ),
+                }
+            ),
+            id="issue365-extreme-datetimes",
+        ),
     ],
 )
 def test_default_dtypes(read_gbq, query, use_bqstorage_api, expected):
     result = read_gbq(query, use_bqstorage_api=use_bqstorage_api)
     pandas.testing.assert_frame_equal(result, expected)
-
-
-#
-#    def test_should_properly_handle_empty_strings(self, project_id):
-#        query = 'SELECT "" AS empty_string'
-#        df = gbq.read_gbq(
-#            query,
-#            project_id=project_id,
-#            credentials=self.credentials,
-#            dialect="legacy",
-#        )
-#        tm.assert_frame_equal(df, DataFrame({"empty_string": [""]}))
-#
-#    def test_should_properly_handle_null_strings(self, project_id):
-#        query = "SELECT STRING(NULL) AS null_string"
-#        df = gbq.read_gbq(
-#            query,
-#            project_id=project_id,
-#            credentials=self.credentials,
-#            dialect="legacy",
-#        )
-#        tm.assert_frame_equal(df, DataFrame({"null_string": [None]}))
-#
-#    def test_should_properly_handle_nullable_integers(self, project_id):
-#        query = """SELECT * FROM
-#                    UNNEST([1, NULL]) AS nullable_integer
-#                """
-#        df = gbq.read_gbq(
-#            query,
-#            project_id=project_id,
-#            credentials=self.credentials,
-#            dialect="standard",
-#            dtypes={"nullable_integer": "Int64"},
-#        )
-#        tm.assert_frame_equal(
-#            df,
-#            DataFrame({"nullable_integer": pandas.Series([1, None], dtype="Int64")}),
-#        )
-#
-#    def test_should_properly_handle_valid_longs(self, project_id):
-#        query = "SELECT 1 << 62 AS valid_long"
-#        df = gbq.read_gbq(
-#            query,
-#            project_id=project_id,
-#            credentials=self.credentials,
-#            dialect="standard",
-#        )
-#        tm.assert_frame_equal(df, DataFrame({"valid_long": [1 << 62]}, dtype="Int64"))
-#
-#    def test_should_properly_handle_nullable_longs(self, project_id):
-#        query = """SELECT * FROM
-#                    UNNEST([1 << 62, NULL]) AS nullable_long
-#                """
-#        df = gbq.read_gbq(
-#            query,
-#            project_id=project_id,
-#            credentials=self.credentials,
-#            dialect="standard",
-#            dtypes={"nullable_long": "Int64"},
-#        )
-#        tm.assert_frame_equal(
-#            df,
-#            DataFrame({"nullable_long": pandas.Series([1 << 62, None], dtype="Int64")}),
-#        )
-#
-#    def test_should_properly_handle_null_integers(self, project_id):
-#        query = "SELECT CAST(NULL AS INT64) AS null_integer"
-#        df = gbq.read_gbq(
-#            query,
-#            project_id=project_id,
-#            credentials=self.credentials,
-#            dialect="standard",
-#            dtypes={"null_integer": "Int64"},
-#        )
-#        tm.assert_frame_equal(
-#            df, DataFrame({"null_integer": pandas.Series([None], dtype="Int64")}),
-#        )
-#
-#    def test_should_properly_handle_valid_floats(self, project_id):
-#        from math import pi
-#
-#        query = "SELECT PI() AS valid_float"
-#        df = gbq.read_gbq(
-#            query,
-#            project_id=project_id,
-#            credentials=self.credentials,
-#            dialect="legacy",
-#        )
-#        tm.assert_frame_equal(df, DataFrame({"valid_float": [pi]}))
-#
-#    def test_should_properly_handle_nullable_floats(self, project_id):
-#        from math import pi
-#
-#        query = """SELECT * FROM
-#                    (SELECT PI() AS nullable_float),
-#                    (SELECT NULL AS nullable_float)"""
-#        df = gbq.read_gbq(
-#            query,
-#            project_id=project_id,
-#            credentials=self.credentials,
-#            dialect="legacy",
-#        )
-#        tm.assert_frame_equal(df, DataFrame({"nullable_float": [pi, None]}))
-#
-#    def test_should_properly_handle_valid_doubles(self, project_id):
-#        from math import pi
-#
-#        query = "SELECT PI() * POW(10, 307) AS valid_double"
-#        df = gbq.read_gbq(
-#            query,
-#            project_id=project_id,
-#            credentials=self.credentials,
-#            dialect="legacy",
-#        )
-#        tm.assert_frame_equal(df, DataFrame({"valid_double": [pi * 10 ** 307]}))
-#
-#    def test_should_properly_handle_nullable_doubles(self, project_id):
-#        from math import pi
-#
-#        query = """SELECT * FROM
-#                    (SELECT PI() * POW(10, 307) AS nullable_double),
-#                    (SELECT NULL AS nullable_double)"""
-#        df = gbq.read_gbq(
-#            query,
-#            project_id=project_id,
-#            credentials=self.credentials,
-#            dialect="legacy",
-#        )
-#        tm.assert_frame_equal(
-#            df, DataFrame({"nullable_double": [pi * 10 ** 307, None]})
-#        )
-#
-#    def test_should_properly_handle_null_floats(self, project_id):
-#        query = """SELECT null_float
-#        FROM UNNEST(ARRAY<FLOAT64>[NULL, 1.0]) AS null_float
-#        """
-#        df = gbq.read_gbq(
-#            query,
-#            project_id=project_id,
-#            credentials=self.credentials,
-#            dialect="standard",
-#        )
-#        tm.assert_frame_equal(df, DataFrame({"null_float": [np.nan, 1.0]}))
-#
-#    def test_should_properly_handle_date(self, project_id):
-#        query = "SELECT DATE(2003, 1, 4) AS date_col"
-#        df = gbq.read_gbq(query, project_id=project_id, credentials=self.credentials,)
-#        expected = DataFrame(
-#            {
-#                "date_col": pandas.Series(
-#                    [datetime.date(2003, 1, 4)], dtype="datetime64[ns]"
-#                )
-#            },
-#        )
-#        tm.assert_frame_equal(df, expected)
-#
-#    def test_should_properly_handle_time(self, project_id):
-#        query = (
-#            "SELECT TIME_ADD(TIME(3, 14, 15), INTERVAL 926589 MICROSECOND) AS time_col"
-#        )
-#        df = gbq.read_gbq(query, project_id=project_id, credentials=self.credentials,)
-#        expected = DataFrame(
-#            {
-#                "time_col": pandas.Series(
-#                    [datetime.time(3, 14, 15, 926589)], dtype="object"
-#                )
-#            },
-#        )
-#        tm.assert_frame_equal(df, expected)
-#
-#    def test_should_properly_handle_timestamp_unix_epoch(self, project_id):
-#        query = 'SELECT TIMESTAMP("1970-01-01 00:00:00") AS unix_epoch'
-#        df = gbq.read_gbq(
-#            query,
-#            project_id=project_id,
-#            credentials=self.credentials,
-#            dialect="legacy",
-#        )
-#        expected = DataFrame(
-#            {"unix_epoch": ["1970-01-01T00:00:00.000000Z"]}, dtype="datetime64[ns]",
-#        )
-#        if expected["unix_epoch"].dt.tz is None:
-#            expected["unix_epoch"] = expected["unix_epoch"].dt.tz_localize("UTC")
-#        tm.assert_frame_equal(df, expected)
-#
-#    def test_should_properly_handle_arbitrary_timestamp(self, project_id):
-#        query = 'SELECT TIMESTAMP("2004-09-15 05:00:00") AS valid_timestamp'
-#        df = gbq.read_gbq(
-#            query,
-#            project_id=project_id,
-#            credentials=self.credentials,
-#            dialect="legacy",
-#        )
-#        expected = DataFrame(
-#            {"valid_timestamp": ["2004-09-15T05:00:00.000000Z"]},
-#            dtype="datetime64[ns]",
-#        )
-#        if expected["valid_timestamp"].dt.tz is None:
-#            expected["valid_timestamp"] = expected["valid_timestamp"].dt.tz_localize(
-#                "UTC"
-#            )
-#        tm.assert_frame_equal(df, expected)
-#
-#    def test_should_properly_handle_datetime_unix_epoch(self, project_id):
-#        query = 'SELECT DATETIME("1970-01-01 00:00:00") AS unix_epoch'
-#        df = gbq.read_gbq(
-#            query,
-#            project_id=project_id,
-#            credentials=self.credentials,
-#            dialect="legacy",
-#        )
-#        tm.assert_frame_equal(
-#            df,
-#            DataFrame({"unix_epoch": ["1970-01-01T00:00:00"]}, dtype="datetime64[ns]"),
-#        )
-#
-#    def test_should_properly_handle_arbitrary_datetime(self, project_id):
-#        query = 'SELECT DATETIME("2004-09-15 05:00:00") AS valid_timestamp'
-#        df = gbq.read_gbq(
-#            query,
-#            project_id=project_id,
-#            credentials=self.credentials,
-#            dialect="legacy",
-#        )
-#        tm.assert_frame_equal(
-#            df, DataFrame({"valid_timestamp": [np.datetime64("2004-09-15T05:00:00")]}),
-#        )
-#
-#    def test_should_properly_handle_null_timestamp(self, project_id):
-#        query = "SELECT TIMESTAMP(NULL) AS null_timestamp"
-#        df = gbq.read_gbq(
-#            query,
-#            project_id=project_id,
-#            credentials=self.credentials,
-#            dialect="legacy",
-#        )
-#        expected = DataFrame({"null_timestamp": [NaT]}, dtype="datetime64[ns]")
-#        expected["null_timestamp"] = expected["null_timestamp"].dt.tz_localize("UTC")
-#        tm.assert_frame_equal(df, expected)
-#
-#    def test_should_properly_handle_null_datetime(self, project_id):
-#        query = "SELECT CAST(NULL AS DATETIME) AS null_datetime"
-#        df = gbq.read_gbq(
-#            query,
-#            project_id=project_id,
-#            credentials=self.credentials,
-#            dialect="standard",
-#        )
-#        tm.assert_frame_equal(df, DataFrame({"null_datetime": [NaT]}))
-#
-#    def test_should_properly_handle_null_boolean(self, project_id):
-#        query = "SELECT BOOLEAN(NULL) AS null_boolean"
-#        df = gbq.read_gbq(
-#            query,
-#            project_id=project_id,
-#            credentials=self.credentials,
-#            dialect="legacy",
-#        )
-#        expected_dtype = "boolean" if FEATURES.pandas_has_boolean_dtype else None
-#        tm.assert_frame_equal(
-#            df, DataFrame({"null_boolean": [None]}, dtype=expected_dtype)
-#        )
-#
-#    def test_should_properly_handle_nullable_booleans(self, project_id):
-#        query = """SELECT * FROM
-#                    (SELECT BOOLEAN(TRUE) AS nullable_boolean),
-#                    (SELECT NULL AS nullable_boolean)"""
-#        df = gbq.read_gbq(
-#            query,
-#            project_id=project_id,
-#            credentials=self.credentials,
-#            dialect="legacy",
-#        )
-#        expected_dtype = "boolean" if FEATURES.pandas_has_boolean_dtype else None
-#        tm.assert_frame_equal(
-#            df, DataFrame({"nullable_boolean": [True, None]}, dtype=expected_dtype)
-#        )
-#
