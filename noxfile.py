ffddacfcaa7fe7f23caddc5cbb5e9ba944eeef86
Anthonios Partheniou <partheniou@google.com>
2023-10-09 11:11:02 -0400

    Update black in noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -25,7 +25,7 @@ import warnings
 
 import nox
 
-BLACK_VERSION = "black==22.3.0"
+BLACK_VERSION = "black==23.7.0"
 ISORT_VERSION = "isort==5.10.1"
 LINT_PATHS = ["docs", "pandas_gbq", "tests", "noxfile.py", "setup.py"]
 
