39cb3017aea3e1038f158834785ebb098b020f07
Tim Swast <swast@google.com>
2024-09-23 10:12:05 -0500

    deps: min pyarrow is now 4.0.0 to support compliant nested types

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ dependencies = [
     "db-dtypes >=1.0.4,<2.0.0",
     "numpy >=1.18.1",
     "pandas >=1.1.4",
-    "pyarrow >=3.0.0",
+    "pyarrow >=4.0.0",
     "pydata-google-auth >=1.5.0",
     # Note: google-api-core and google-auth are also included via transitive
     # dependency on google-cloud-bigquery, but this library also uses them
