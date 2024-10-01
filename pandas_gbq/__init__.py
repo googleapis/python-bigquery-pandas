fbb22ba4cd768bee0446437f54f58f3d71123ef6
Chalmer Lowe <chalmerlowe@google.com>
2024-10-01 10:41:39 -0400

    Update pandas_gbq/__init__.py

--- pandas_gbq/__init__.py
+++ pandas_gbq/__init__.py
@@ -2,6 +2,8 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+import warnings
+
 from pandas_gbq import version as pandas_gbq_version
 
 from . import _versions_helpers
