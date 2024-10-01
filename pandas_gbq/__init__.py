2449ab2bdcd7843fb59804baac1d609a1204c692
chalmer lowe <chalmerlowe@google.com>
2024-10-01 14:34:50 +0000

    chore: adds deprecation warnings related to Python 3.7 3.8

--- pandas_gbq/__init__.py
+++ pandas_gbq/__init__.py
@@ -6,6 +6,19 @@ from pandas_gbq import version as pandas_gbq_version
 
 from .gbq import Context, context, read_gbq, to_gbq  # noqa
 
+from . import _versions_helpers
+
+sys_major, sys_minor, sys_micro = _versions_helpers.extract_runtime_version()
+if sys_major == 3 and sys_minor in (7, 8):
+    warnings.warn(
+        "The python-bigquery library will stop supporting Python 3.7 "
+        "and Python 3.8 in a future major release expected in Q4 2024. "
+        f"Your Python version is {sys_major}.{sys_minor}.{sys_micro}. We "
+        "recommend that you update soon to ensure ongoing support. For "
+        "more details, see: [Google Cloud Client Libraries Supported Python Versions policy](https://cloud.google.com/python/docs/supported-python-versions)",
+        PendingDeprecationWarning,
+    )
+
 __version__ = pandas_gbq_version.__version__
 
 __all__ = [
