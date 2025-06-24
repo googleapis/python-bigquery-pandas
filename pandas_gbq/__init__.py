5fea48ff330dafee2b2915c60a6f89c8c375ec93
Chalmer Lowe <chalmerlowe@google.com>
2025-06-24 06:50:01 -0400

    Update pandas_gbq/__init__.py

--- pandas_gbq/__init__.py
+++ pandas_gbq/__init__.py
@@ -13,8 +13,8 @@ from .gbq import read_gbq, to_gbq  # noqa
 sys_major, sys_minor, sys_micro = _versions_helpers.extract_runtime_version()
 if sys_major == 3 and sys_minor < 9:
     warnings.warn(
-        "pandas-gbq will stop supporting Python versions older than 3.9 "
-        "in a future release. Your Python version is "
+        "pandas-gbq no longer supports Python versions older than 3.9. "
+        "Your Python version is "
         f"{sys_major}.{sys_minor}.{sys_micro}. Please update "
         "to Python 3.9 or newer to ensure ongoing support. For more details, "
         "see: https://cloud.google.com/python/docs/supported-python-versions",
